d37583c851ba6ccac2a397e8e404fd6d7203ef60
Beksultan <s_zhumatay@kbtu.kz>
2024-03-01 18:02:52 +0600

    schema

--- database/database.go
+++ database/database.go
@@ -1,1 +1,20 @@
+package database
 
+import (
+	"bank-app/helpers"
+
+	"github.com/jinzhu/gorm"
+)
+
+// Create global variable
+var DB *gorm.DB
+
+// Create InitDatabase function
+func InitDatabase() {
+	database, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=bank-app password=19660827 sslmode=disable")
+	helpers.HandleErr(err)
+	// Set up connection pool
+	database.DB().SetMaxIdleConns(20)
+	database.DB().SetMaxOpenConns(200)
+	DB = database
+}
