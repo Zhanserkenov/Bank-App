917cc39b01d2672a902c83e760ae1f4e082b4549
Zhanserkenov Arsen <73212575+Zhanserkenov@users.noreply.github.com>
2024-03-01 19:07:59 +0600

    Update interfaces.go

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,1 +1,25 @@
+package interfaces
 
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Username string
+	Email    string
+	Password string
+}
+
+type Account struct {
+	gorm.Model
+	Type    string
+	Name    string
+	Balance uint
+	UserID  uint
+}
+
+type Transaction struct {
+	gorm.Model
+	From   uint
+	To     uint
+	Amount int
+}
