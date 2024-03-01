d46dcade22f41ef101fb058617e15d122853e5a5
sayana8 <122979736+sayana8@users.noreply.github.com>
2024-03-01 19:14:22 +0600

    Update interfaces.go

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -17,9 +17,40 @@ type Account struct {
 	UserID  uint
 }
 
+// Create transaction interface
 type Transaction struct {
 	gorm.Model
 	From   uint
 	To     uint
 	Amount int
+}
+
+type ResponseTransaction struct {
+	ID     uint
+	From   uint
+	To     uint
+	Amount int
+}
+
+type ResponseAccount struct {
+	ID      uint
+	Name    string
+	Balance int
+}
+
+type ResponseUser struct {
+	ID       uint
+	Username string
+	Email    string
+	Accounts []ResponseAccount
+}
+
+// Create Validation interface
+type Validation struct {
+	Value string
+	Valid string
+}
+
+type ErrResponse struct {
+	Message string
 }
