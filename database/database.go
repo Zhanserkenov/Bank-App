d1e22ac1b5a5672279615db7bb1f9e5917dfe266
Zhanserkenov Arsen <73212575+Zhanserkenov@users.noreply.github.com>
2024-03-02 14:10:21 +0500

    Update database.go

--- database/database.go
+++ database/database.go
@@ -1,40 +1,20 @@
 package database
 
 import (
-	"bank-app/interfaces"
+	"bank-app/helpers"
+
 	"github.com/jinzhu/gorm"
 )
 
+// Create global variable
 var DB *gorm.DB
 
-func ConnectDatabase() (*gorm.DB, error) {
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=bank-app password=19660827 sslmode=disable")
-	if err != nil {
-		return nil, err
-	}
-	db.DB().SetMaxIdleConns(20)
-	db.DB().SetMaxOpenConns(200)
-	return db, nil
-}
-
-func CreateUser(user *interfaces.User) error {
-	return DB.Create(user).Error
-}
-
-func CreateAccount(account *interfaces.Account) error {
-	return DB.Create(account).Error
-}
-
-func GetUserByID(id uint) (*interfaces.User, error) {
-	user := &interfaces.User{}
-	err := DB.First(user, id).Error
-	return user, err
-}
-
-func UpdateUser(user *interfaces.User) error {
-	return DB.Save(user).Error
-}
-
-func DeleteUser(user *interfaces.User) error {
-	return DB.Delete(user).Error
+// Create InitDatabase function
+func InitDatabase() {
+	database, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=bank-app password=19660827 sslmode=disable")
+	helpers.HandleErr(err)
+	// Set up connection pool
+	database.DB().SetMaxIdleConns(20)
+	database.DB().SetMaxOpenConns(200)
+	DB = database
 }
